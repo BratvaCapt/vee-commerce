812405d65ecf3cf6bb36d6b5b80df930e805795f
Eddy <eddydizey@gmail.com>
2020-12-11 02:04:40 -0800

    blog app

---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import  {Link} from 'react-router-dom';
+import logo from '../logo.jpg';
+import styled from 'styled-components'
+import { ButtonContainer } from './Button';
+
+export default function Navbar() {
+
+        return (
+            <NavWrapper className='navbar navbar-expand-sm navbar-dark px-sm-5'>
+        <Link to='/'>
+        <img src={logo} width ='45px'  alt='store' className='navbar-brand' />
+        </Link> 
+        <ul className='navbar-nav align-items-center'>
+      <li className='nav-items ml-5'>
+    <Link to='/' className='nav-link'>
+    Products
+    </Link>
+      </li>
+        </ul>
+        <Link to='/cart' className='ml-auto'>
+            <ButtonContainer>
+               <span className='mr-2'>
+               <i className='fas fa-cart-plus'></i>   
+              </span> 
+                My Cart
+            </ButtonContainer>
+        </Link>
+            </NavWrapper>
+        );
+    
+    }
+
+ const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+  .nav-link{
+    color: var(--mainWhite)!important;
+    font-size:1.3rems;
+    text-transform: capitalize;
+  }
+ `
---
+++ src/data.js
@@ -0,0 +1,110 @@
+export const storeProducts = [
+    {
+      id:1,
+      title: 'Google Pixel',
+      img: 'images/product-10.jpg',
+      price: 10,
+      company: 'GOOGLE',
+      info:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quas nemo, rerum accusantium corporis dolor ullam officia maiores rem sapiente. Numquam minus fugit optio similique dolorem veritatis laudantium aliquam eveniet, modi natus unde fuga accusamus et doloremque, facere temporibus repellendus. Expedita ratione, odio sit consectetur ullam saepe molestiae adipisci facilis molestias alias! Eveniet in eius nam excepturi. Harum assumenda quod mollitia esse, ab nostrum voluptas cum fuga neque quia repudiandae ratione, ea accusamus facere laborum voluptatum soluta facilis. Veniam iure natus rem at obcaecati enim fugiat amet eligendi, modi accusantium sit odio, voluptatem dicta ea repellendus aliquid ipsam in magnam.'
+      ,
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+        id:2,
+        title: 'Samsung 23',
+        img: 'images/product-2.jpg',
+        price: 15,
+        company: 'SAMSUNG',
+        info:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quas nemo, rerum accusantium corporis dolor ullam officia maiores rem sapiente. Numquam minus fugit optio similique dolorem veritatis laudantium aliquam eveniet, modi natus unde fuga accusamus et doloremque, facere temporibus repellendus. Expedita ratione, odio sit consectetur ullam saepe molestiae adipisci facilis molestias alias! Eveniet in eius nam excepturi. Harum assumenda quod mollitia esse, ab nostrum voluptas cum fuga neque quia repudiandae ratione, ea accusamus facere laborum voluptatum soluta facilis. Veniam iure natus rem at obcaecati enim fugiat amet eligendi, modi accusantium sit odio, voluptatem dicta ea repellendus aliquid ipsam in magnam.'
+        ,
+        inCart: false,
+        count: 0,
+        total: 0
+      },
+      {
+        id:3,
+        title: 'I Pixel',
+        img: 'images/product-3.jpg',
+        price: 40,
+        company: 'APPLE',
+        info:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quas nemo, rerum accusantium corporis dolor ullam officia maiores rem sapiente. Numquam minus fugit optio similique dolorem veritatis laudantium aliquam eveniet, modi natus unde fuga accusamus et doloremque, facere temporibus repellendus. Expedita ratione, odio sit consectetur ullam saepe molestiae adipisci facilis molestias alias! Eveniet in eius nam excepturi. Harum assumenda quod mollitia esse, ab nostrum voluptas cum fuga neque quia repudiandae ratione, ea accusamus facere laborum voluptatum soluta facilis. Veniam iure natus rem at obcaecati enim fugiat amet eligendi, modi accusantium sit odio, voluptatem dicta ea repellendus aliquid ipsam in magnam.'
+        ,
+        inCart: false,
+        count: 0,
+        total: 0
+      },
+      {
+        id:4,
+        title: 'Infinix',
+        img: 'images/product-9.jpg',
+        price: 25,
+        company: 'TECNO',
+        info:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quas nemo, rerum accusantium corporis dolor ullam officia maiores rem sapiente. Numquam minus fugit optio similique dolorem veritatis laudantium aliquam eveniet, modi natus unde fuga accusamus et doloremque, facere temporibus repellendus. Expedita ratione, odio sit consectetur ullam saepe molestiae adipisci facilis molestias alias! Eveniet in eius nam excepturi. Harum assumenda quod mollitia esse, ab nostrum voluptas cum fuga neque quia repudiandae ratione, ea accusamus facere laborum voluptatum soluta facilis. Veniam iure natus rem at obcaecati enim fugiat amet eligendi, modi accusantium sit odio, voluptatem dicta ea repellendus aliquid ipsam in magnam.'
+        ,
+        inCart: false,
+        count: 0,
+        total: 0
+      },
+      {
+        id:5,
+        title: 'Nokia',
+        img: 'images/product-5.jpg',
+        price: 10,
+        company: 'NOKIA',
+        info:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quas nemo, rerum accusantium corporis dolor ullam officia maiores rem sapiente. Numquam minus fugit optio similique dolorem veritatis laudantium aliquam eveniet, modi natus unde fuga accusamus et doloremque, facere temporibus repellendus. Expedita ratione, odio sit consectetur ullam saepe molestiae adipisci facilis molestias alias! Eveniet in eius nam excepturi. Harum assumenda quod mollitia esse, ab nostrum voluptas cum fuga neque quia repudiandae ratione, ea accusamus facere laborum voluptatum soluta facilis. Veniam iure natus rem at obcaecati enim fugiat amet eligendi, modi accusantium sit odio, voluptatem dicta ea repellendus aliquid ipsam in magnam.'
+        ,
+        inCart: false,
+        count: 0,
+        total: 0
+      },
+      {
+        id:6,
+        title: 'I mac Pro',
+        img: 'images/product-6.jpg',
+        price: 30,
+        company: 'MAC',
+        info:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quas nemo, rerum accusantium corporis dolor ullam officia maiores rem sapiente. Numquam minus fugit optio similique dolorem veritatis laudantium aliquam eveniet, modi natus unde fuga accusamus et doloremque, facere temporibus repellendus. Expedita ratione, odio sit consectetur ullam saepe molestiae adipisci facilis molestias alias! Eveniet in eius nam excepturi. Harum assumenda quod mollitia esse, ab nostrum voluptas cum fuga neque quia repudiandae ratione, ea accusamus facere laborum voluptatum soluta facilis. Veniam iure natus rem at obcaecati enim fugiat amet eligendi, modi accusantium sit odio, voluptatem dicta ea repellendus aliquid ipsam in magnam.'
+        ,
+        inCart: false,
+        count: 0,
+        total: 0
+      },
+      {
+        id:7,
+        title: 'Tecno Camon X',
+        img: 'images/product-7.jpg',
+        price: 10,
+        company: 'TECNO',
+        info:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quas nemo, rerum accusantium corporis dolor ullam officia maiores rem sapiente. Numquam minus fugit optio similique dolorem veritatis laudantium aliquam eveniet, modi natus unde fuga accusamus et doloremque, facere temporibus repellendus. Expedita ratione, odio sit consectetur ullam saepe molestiae adipisci facilis molestias alias! Eveniet in eius nam excepturi. Harum assumenda quod mollitia esse, ab nostrum voluptas cum fuga neque quia repudiandae ratione, ea accusamus facere laborum voluptatum soluta facilis. Veniam iure natus rem at obcaecati enim fugiat amet eligendi, modi accusantium sit odio, voluptatem dicta ea repellendus aliquid ipsam in magnam.'
+        ,
+        inCart: false,
+        count: 0,
+        total: 0
+      },
+      {
+        id:8,
+        title: 'Gionee',
+        img: 'images/product-1.jpg',
+        price: 10,
+        company: 'GIONEE',
+        info:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quas nemo, rerum accusantium corporis dolor ullam officia maiores rem sapiente. Numquam minus fugit optio similique dolorem veritatis laudantium aliquam eveniet, modi natus unde fuga accusamus et doloremque, facere temporibus repellendus. Expedita ratione, odio sit consectetur ullam saepe molestiae adipisci facilis molestias alias! Eveniet in eius nam excepturi. Harum assumenda quod mollitia esse, ab nostrum voluptas cum fuga neque quia repudiandae ratione, ea accusamus facere laborum voluptatum soluta facilis. Veniam iure natus rem at obcaecati enim fugiat amet eligendi, modi accusantium sit odio, voluptatem dicta ea repellendus aliquid ipsam in magnam.'
+        ,
+        inCart: false,
+        count: 0,
+        total: 0
+      }
+]
+export const detailProduct = {
+    id:1,
+    title: 'Google Pixel - Black',
+    img: 'images/product-8.jpg',
+    price: 10,
+    company: 'GOOGLE',
+    info:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus quas nemo, rerum accusantium corporis dolor ullam officia maiores rem sapiente. Numquam minus fugit optio similique dolorem veritatis laudantium aliquam eveniet, modi natus unde fuga accusamus et doloremque, facere temporibus repellendus. Expedita ratione, odio sit consectetur ullam saepe molestiae adipisci facilis molestias alias! Eveniet in eius nam excepturi. Harum assumenda quod mollitia esse, ab nostrum voluptas cum fuga neque quia repudiandae ratione, ea accusamus facere laborum voluptatum soluta facilis. Veniam iure natus rem at obcaecati enim fugiat amet eligendi, modi accusantium sit odio, voluptatem dicta ea repellendus aliquid ipsam in magnam.'
+    ,
+    inCart: false,
+    count: 0,
+    total: 0
+  }
